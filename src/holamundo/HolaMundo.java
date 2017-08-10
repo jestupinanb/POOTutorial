b93698a5c70f7896bf18228cb755065d4b008e42
Estudiante <Estudiante@10.203.1.108>
2017-08-10 14:49:02 -0500

    Prog Clase

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -15,7 +15,7 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo 12354");
         for(int i=0;i<3;i++){
             System.out.println("Hola mundoo");
         };
